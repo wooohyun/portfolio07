d783eefb71bda1d83eca6a54ef039ad4fa2f52ce
wooohyun <woodoll543@gmail.com>
2022-08-25 16:50:49 +0900

    20220825

--- js/main.js
+++ js/main.js
@@ -1,79 +1,93 @@
 function myPlugin({ swiper, extendParams, on }) {
-    extendParams({
-      debugger: false,
-    });
+  extendParams({
+    debugger: false,
+  });
 
-    on('init', () => {
-      if (!swiper.params.debugger) return;
-      console.log('init');
-    });
-    on('click', (swiper, e) => {
-      if (!swiper.params.debugger) return;
-      console.log('click');
-    });
-    on('tap', (swiper, e) => {
-      if (!swiper.params.debugger) return;
-      console.log('tap');
-    });
-    on('doubleTap', (swiper, e) => {
-      if (!swiper.params.debugger) return;
-      console.log('doubleTap');
-    });
-    on('sliderMove', (swiper, e) => {
-      if (!swiper.params.debugger) return;
-      console.log('sliderMove');
-    });
-    on('slideChange', () => {
-      if (!swiper.params.debugger) return;
-      console.log(
-        'slideChange',
-        swiper.previousIndex,
-        '->',
-        swiper.activeIndex
-      );
-    });
-    on('slideChangeTransitionStart', () => {
-      if (!swiper.params.debugger) return;
-      console.log('slideChangeTransitionStart');
-    });
-    on('slideChangeTransitionEnd', () => {
-      if (!swiper.params.debugger) return;
-      console.log('slideChangeTransitionEnd');
-    });
-    on('transitionStart', () => {
-      if (!swiper.params.debugger) return;
-      console.log('transitionStart');
-    });
-    on('transitionEnd', () => {
-      if (!swiper.params.debugger) return;
-      console.log('transitionEnd');
-    });
-    on('fromEdge', () => {
-      if (!swiper.params.debugger) return;
-      console.log('fromEdge');
-    });
-    on('reachBeginning', () => {
-      if (!swiper.params.debugger) return;
-      console.log('reachBeginning');
-    });
-    on('reachEnd', () => {
-      if (!swiper.params.debugger) return;
-      console.log('reachEnd');
-    });
-  };
+  on('init', () => {
+    if (!swiper.params.debugger) return;
+    console.log('init');
+  });
+  on('click', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('click');
+  });
+  on('tap', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('tap');
+  });
+  on('doubleTap', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('doubleTap');
+  });
+  on('sliderMove', (swiper, e) => {
+    if (!swiper.params.debugger) return;
+    console.log('sliderMove');
+  });
+  on('slideChange', () => {
+    if (!swiper.params.debugger) return;
+    console.log(
+      'slideChange',
+      swiper.previousIndex,
+      '->',
+      swiper.activeIndex
+    );
+  });
+  on('slideChangeTransitionStart', () => {
+    if (!swiper.params.debugger) return;
+    console.log('slideChangeTransitionStart');
+  });
+  on('slideChangeTransitionEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('slideChangeTransitionEnd');
+  });
+  on('transitionStart', () => {
+    if (!swiper.params.debugger) return;
+    console.log('transitionStart');
+  });
+  on('transitionEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('transitionEnd');
+  });
+  on('fromEdge', () => {
+    if (!swiper.params.debugger) return;
+    console.log('fromEdge');
+  });
+  on('reachBeginning', () => {
+    if (!swiper.params.debugger) return;
+    console.log('reachBeginning');
+  });
+  on('reachEnd', () => {
+    if (!swiper.params.debugger) return;
+    console.log('reachEnd');
+  });
+};
 
-    // Init Swiper
-    var swiper = new Swiper('.swiper', {
-      // Install Plugin To Swiper
-      modules: [myPlugin],
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      // Enable debugger
-      debugger: true,
-    });
+// Init Swiper
+var swiper = new Swiper('.main_swipe', {
+  // Install Plugin To Swiper
+  modules: [myPlugin],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Enable debugger
+  debugger: true,
+});
+
+var swiper = new Swiper('.product_lineup', {
+});
+
+var swiper = new Swiper(".main_vt", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
